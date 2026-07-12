0904fb021e436a6a015202f09adf7cf463f9ca69
Saim Ramzan <brushandcodes@gmail.com>
2026-07-12 13:07:30 +0500

    Created a function to get the Computer's choice

---
+++ game.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    const randomNumber = Math.random();
+    if (randomNumber < 1 / 3) {
+        return "rock";
+    } else if (randomNumber < 2 / 3) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
