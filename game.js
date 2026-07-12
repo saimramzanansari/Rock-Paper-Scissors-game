2e6ad07b34e26ab8ec3d0267ef33fdb56c0841d5
Saim Ramzan <brushandcodes@gmail.com>
2026-07-12 13:46:26 +0500

    Created a function to get the human's choice

--- game.js
+++ game.js
@@ -8,3 +8,8 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+function getHumanChoice() {
+    const humanChoice = prompt("Enter your choice: rock, paper, or scissors");
+    return humanChoice.toLowerCase();
+}
